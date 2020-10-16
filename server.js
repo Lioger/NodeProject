15f11ae41d3b950327d09d42d628465c38d468a3
Nick <nikstpnov@gmail.com>
2020-10-16 17:41:37 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log('request made');
+
+    res.setHeader('Content-Type', 'text/html');
+
+    let path = './views/';
+    switch (req.url) {
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about');
+            res.end();
+            break;
+        default :
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.end(data);
+        }
+
+    })
+
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening 4 req on port 3000')
+});
+
